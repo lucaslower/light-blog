1eae38a9651d65610721fb2ba790bf939571b214
Lucas Lower <lucas@lucaslower.com>
2019-03-18 11:50:02 -0500

    fixes

--- views/default/layout/header.php
+++ views/default/layout/header.php
@@ -23,8 +23,7 @@ $header_class = $full_header ? 'full' : '';
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="icon" type="image/png" href="https://<?php echo $app->get_setting('site.url'); ?>/views/default/assets/favicon.png" />
 		<link rel="stylesheet" href="https://<?php echo $app->get_setting('site.url'); ?>/views/css.php">
-		<link href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono:400,400i|IBM+Plex+Sans:300,300i,400,400i,600,700|IBM+Plex+Serif:400,400i,700,700i" rel="stylesheet">
-		<link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700|Playfair+Display+SC:400,700,900" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono:400,400i|IBM+Plex+Sans:300,300i,400,400i,600,700" rel="stylesheet">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 	</head>
 	<body>
@@ -32,12 +31,13 @@ $header_class = $full_header ? 'full' : '';
 		<header id="page_top" class="page_width <?php echo $header_class; ?>">
 			<section id="navigation">
 				<nav>
-					<a href="https://nataleemae.com">Home</a>
-					<a href="https://nataleemae.com/about">About</a>
+					<a href="https://lucaslower.com">Home</a>
+					<a href="https://lucaslower.com/about">About</a>
+					<a href="https://lucaslower.com/projects">Projects</a>
 				</nav>
 				<nav>
-					<a href="https://www.instagram.com/natalee.mae/"><i class="fab fa-instagram"></i></a>
-					<a href="mailto:nataleerr@gmail.com"><i class="fas fa-envelope"></i></a>
+					<a href="https://github.com/lucaslower"><i class="fab fa-github"></i></a>
+					<a href="mailto:lucas@lucaslower.com"><i class="fas fa-envelope"></i></a>
 				</nav>
 			</section>
 
